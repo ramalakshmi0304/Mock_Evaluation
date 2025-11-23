02d3ee9610c8375197b0e4f3887cb1fd6a4fdb57
ramalakshmi0304 <mlaksmipradeep@gmail.com>
2025-11-23 20:21:39 +0530

    part_C

---
+++ Q3_PartC/Q10_simple_closure/index.js
@@ -0,0 +1,22 @@
+let count=0;
+function counter(){
+    return{
+
+    function: c()
+    console.log(count)
+
+}
+return count;
+
+}
+
+
+
+
+
+
+
+const c = counter();
+c(); // 1
+c(); // 2
+c(); // 3
---
+++ Q3_PartC/Q11_Medium_closure/index.js
@@ -0,0 +1,12 @@
+let balance=0;
+function createWallet(){
+    return{
+        function:addMoney(amount);
+        myWallet.push(amount)
+
+        function.
+
+
+
+    }
+}
