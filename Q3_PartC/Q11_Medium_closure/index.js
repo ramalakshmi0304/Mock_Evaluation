2ccfe52dfb728e4f50002d1e33f361c230695a64
ramalakshmi0304 <mlaksmipradeep@gmail.com>
2025-11-23 20:29:03 +0530

    part_C_changesdone

--- Q3_PartC/Q11_Medium_closure/index.js
+++ Q3_PartC/Q11_Medium_closure/index.js
@@ -1,12 +1,22 @@
-let balance=0;
-function createWallet(){
-    return{
-        function:addMoney(amount);
+let balance = 0;
+function createWallet() {
+    return {
+        function: addMoney(amount){
         myWallet.push(amount)
+        myWallet+=amount
+    }
 
-        function.
-
-
+    function: checkBalance(myWallet){
+        console.log(myWallet)
 
     }
 }
+return checkBalance{
+    console.log(checkBalance)
+}
+}
+
+let myWallet = createWallet();
+myWallet.addMoney(500);
+myWallet.addMoney(200);
+myWallet.checkBalance(); 
