e19f822c8ed29da6aed5f00ce9b17fe38aeb007b
ramalakshmi0304 <mlaksmipradeep@gmail.com>
2025-11-23 20:12:45 +0530

    part_B

---
+++ Q2_PartB/Q2.1_inbuilt_HOF/index.js
@@ -0,0 +1,8 @@
+let nums = [10, 3, 7, 20, 13, 2];
+
+const squared=nums.map=(num)=> num*num;
+
+console.log(squared)
+
+const filtered= mapped.filter=(nums)=> nums%2===1
+const 
---
+++ Q2_PartB/Q2.2_callback_fn/index.js
@@ -0,0 +1,24 @@
+
+function: displayCar(){
+    console.log()
+}
+
+
+function: displayTruck(){
+    console.log()
+}
+
+function: displayBike(){
+    console.log()
+    function vehicleInfo(vehicle,fn ){
+
+        console.log()
+    }
+
+
+const vehicleInfo=(vehicleCategory, callbackFn)
+
+vehicleInfo("Car", displayCar)
+vehicleInfo("Truck", displayTruck)
+vehicleInfo("Bike", displayBike)
+
